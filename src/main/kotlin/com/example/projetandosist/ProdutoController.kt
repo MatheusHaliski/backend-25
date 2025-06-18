af464a06e7e8ec198cbbdc78541242db323d5351
MatheusHaliski <120330198+MatheusHaliski@users.noreply.github.com>
2025-06-18 16:29:01 -0300

    Update ProdutoController.kt

--- src/main/kotlin/com/example/projetandosist/ProdutoController.kt
+++ src/main/kotlin/com/example/projetandosist/ProdutoController.kt
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.net.URL
 import kotlin.io.encoding.ExperimentalEncodingApi
+import org.springframework.web.bind.annotation.CrossOrigin
 
 @RestController
 @RequestMapping("/produtos")

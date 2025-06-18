0d71614a4c8ab45f579bef110b1829afe641b991
MatheusHaliski <120330198+MatheusHaliski@users.noreply.github.com>
2025-06-18 17:00:12 -0300

    Update SecurityConfig.kt

--- src/main/kotlin/com/example/projetandosist/SecurityConfig.kt
+++ src/main/kotlin/com/example/projetandosist/SecurityConfig.kt
@@ -1,18 +1,3 @@
-package com.example.projetandosist
-
-import com.example.projetandosist.security.JwtFilter
-import com.example.projetandosist.security.JwtUtil
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.CorsConfigurationSource
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
-import java.util.*
-
 @Configuration
 class SecurityConfig(
     private val jwtUtil: JwtUtil,
@@ -27,19 +12,20 @@ class SecurityConfig(
             .headers { it.frameOptions { frame -> frame.sameOrigin() } }
             .authorizeHttpRequests { auth ->
                 auth
-                    .requestMatchers(AntPathRequestMatcher("/auth/**")).permitAll()
-                    .requestMatchers(AntPathRequestMatcher("/pessoas/**")).permitAll()
-                    .requestMatchers(AntPathRequestMatcher("/produtos/**")).permitAll()
-                    .requestMatchers(AntPathRequestMatcher("/doacoes/**")).permitAll()
-                    .requestMatchers(AntPathRequestMatcher("/login.html")).permitAll()
-                    .requestMatchers(AntPathRequestMatcher("/perfil")).permitAll()
-                    .requestMatchers(AntPathRequestMatcher("/relatorio/**")).permitAll()
                     .requestMatchers(
-                        AntPathRequestMatcher("/swagger-ui/**"),
-                        AntPathRequestMatcher("/v3/api-docs/**")
+                        "/auth/**",
+                        "/pessoas",
+                        "/pessoas/**",
+                        "/produtos/**",
+                        "/doacoes/**",
+                        "/login.html",
+                        "/perfil",
+                        "/relatorio/**",
+                        "/swagger-ui/**",
+                        "/v3/api-docs/**",
+                        "/h2-console/**"
                     ).permitAll()
-                    .requestMatchers(AntPathRequestMatcher("/h2-console/**")).permitAll()
-                    .requestMatchers(AntPathRequestMatcher("/admin/**")).hasAuthority("USUARIO_ADM")
+                    .requestMatchers("/admin/**").hasAuthority("USUARIO_ADM")
                     .anyRequest().authenticated()
             }
             .addFilterBefore(

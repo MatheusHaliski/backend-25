8408264e369d06a96e44809ff2a32332afbb7264
MatheusHaliski <120330198+MatheusHaliski@users.noreply.github.com>
2025-06-18 17:14:47 -0300

    Update SecurityConfig.kt

--- src/main/kotlin/com/example/projetandosist/SecurityConfig.kt
+++ src/main/kotlin/com/example/projetandosist/SecurityConfig.kt
@@ -1,3 +1,19 @@
+package com.example.projetandosist
+
+import com.example.projetandosist.security.JwtFilter
+import com.example.projetandosist.security.JwtUtil
+import com.example.projetandosist.PessoaRepository
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+
 @Configuration
 class SecurityConfig(
     private val jwtUtil: JwtUtil,
